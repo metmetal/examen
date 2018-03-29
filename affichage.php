127d12e8c8cdde68e57a4d56638dd0d1d996acf2
metmetal <metzopro97@gmail.com>
2018-03-29 19:08:20 +0200

    mon examen

---
+++ affichage.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Lister Les elements de la base de donnée</title>
+  <META charset="utf-8"/>
+</head>
+<body>
+   <?php
+   try{
+    require('connection.php');
+    $requete = $con->query("SELECT * from personne");
+    ?>
+    <table>
+    <tr>
+      <td colspan="6" id="entete">Listes des clients</td>
+    </tr>
+    <p><tr>
+        <th>ID</th>
+      <th>NOM</th>
+      <th>PRENOM</th>
+      <th>ADRESSE</th>
+      
+
+    </tr>
+    </p>
+
+
+      <?php
+             while($resultat = $requete->fetch()){
+        ?>
+                <tr>
+                 <p> <td><?php echo $resultat['ID']; ?></td>
+                  <td><?php echo $resultat['NOM']; ?></td>
+                  <td><?php echo $resultat['PRENOM'];  ?></td>
+                  <td><?php echo $resultat['ADRESSE'];  ?></td>
+                
+                
+                 </p>
+                </tr>
+
+        <?php
+             }
+         ?>
+
+    </table>
+    <?php
+   }catch(PDOException $e){
+         die('Erreur!!!'.$e->getMessage());
+   }
+   ?>
+
+</body>
+</html>
---
+++ connexionBD.php
@@ -0,0 +1,10 @@
+<?php
+  
+  $server="localhost";
+  $login ="root";
+  $passwd = "";
+  $dbname="school";
+  // Création d'un objet de connexion à la base de données
+  	$con = new PDO('mysql:host='.$server.';dbname='.$dbname.';charset=utf8',$login,$passwd);
+?>
+conexionBD
